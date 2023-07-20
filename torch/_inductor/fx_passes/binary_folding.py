e1f6e86f3d300ea62bac963a5d42c17dbd42b8ce
XiaobingSuper <xiaobing.zhang@intel.com>
2023-07-20 03:11:09 -0400

    inductor: using binary folding path to do conv+bn folding
    ghstack-source-id: 8af6adf06459905ecebceb166ebce06907593067
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/105650

--- torch/_inductor/fx_passes/binary_folding.py
+++ torch/_inductor/fx_passes/binary_folding.py
@@ -39,11 +39,20 @@ def binary_folding_init():
         other_shape = other_tensor.shape
         if len(weight_shape) < len(other_shape):
             return False
-        for i in reversed(range(len(other_shape))):
-            if i == 1 and weight_shape[0] == other_shape[i]:
-                continue
-            if other_shape[i] != 1:
-                return False
+        if len(weight_shape) == len(other_shape) + 1:
+            # weight shape is [o, i, *], other_shape is [o, 1...].
+            for i in reversed(range(len(other_shape))):
+                if i == 0 and weight_shape[0] == other_shape[i]:
+                    continue
+                if other_shape[i] != 1:
+                    return False
+        else:
+            # weight shape is [o, i, *], other_shape is [1, i, *]
+            for i in reversed(range(len(other_shape))):
+                if i == 1 and weight_shape[0] == other_shape[i]:
+                    continue
+                if other_shape[i] != 1:
+                    return False
         return True
 
     def _check_conv_and_broadcast_op(conv_node, other):
